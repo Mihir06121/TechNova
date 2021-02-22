c9f5eb86e70f9924405b40d149aa8072972390f8
Mihir06121 <mp06121@gmail.com>
2021-02-22 20:30:39 +0530

    Contact Section

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -13,7 +13,11 @@ const Contact = () => {
                         <Card className="bg-dark" body>
                         <div align="center" className="pt-5 pb-5">
                             <h1 className="pt-5 pb-5">Technical Day</h1>
-                            <h3>Contact</h3>
+                            <h3 className="pb-3">Contact</h3>
+                            <h4>
+                            Mihir Panchal <br/>
+                            7700983123
+                            </h4>
                         </div>
                         </Card>
                     </Col>
@@ -21,7 +25,11 @@ const Contact = () => {
                         <Card className="bg-dark" body>
                         <div align="center" className="pt-5 pb-5">
                             <h1 className="pt-5 pb-5">Webinar</h1>
-                            <h3>Contact</h3>
+                            <h3 className="pb-3">Contact</h3>
+                            <h4>
+                                Izhar Sayed<br/>
+                                90827 51010
+                            </h4>
                         </div>
                         </Card>
                     </Col>
@@ -29,7 +37,21 @@ const Contact = () => {
                         <Card className="bg-dark" body>
                         <div align="center" className="pt-5 pb-5">
                             <h1 className="pt-5 pb-5">E-Sports</h1>
-                            <h3>Contact</h3>
+                            <h3 className="pb-3">Contact</h3>
+                            <Row>
+                                <Col>
+                                    <h4>
+                                        Ketan Rathod<br/>
+                                        86899 05216
+                                    </h4>
+                                </Col>
+                                <Col>
+                                    <h4>
+                                        Izhar Sayed<br/>
+                                        90827 51010
+                                    </h4>
+                                </Col>
+                            </Row>
                         </div>
                         </Card>
                     </Col>
