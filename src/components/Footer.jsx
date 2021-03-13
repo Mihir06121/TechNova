2a2310f9063d80333217c2143c663e23fd0a4b5f
Mihir <mp06121@gmail.com>
2021-03-13 14:46:22 +0530

    footer changed

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,13 +5,13 @@ const Footer =() => {
         <div align="center" className="pb-5">
             <footer className="footer">
                 <div className="container text-white">
-                <div className="container">Copyright © <a rel="noopener noreferrer" target="_blank"
+                {/* <div className="container">Copyright © <a rel="noopener noreferrer" target="_blank"
                     href="https://technova21.netlify.app/">technova21.netlify.app</a> 2021
-                </div>
-                {/* <div className="container">
+                </div> */}
+                <div className="container">
                     Developed and maintained by Mihir Panchal <a rel="noopener noreferrer" target="_blank"
                     href="https://mihirpanchal.com/">mihirpanchal.com</a>
-                </div> */}
+                </div>
                 </div>
             </footer>
         </div>
